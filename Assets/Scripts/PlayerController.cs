6f9be6524aac023504bf1aa71bb1a118c12a335a
Anastasiia A. <95031679+anastasiiaal@users.noreply.github.com>
2026-04-30 15:36:35 +0200

    feat: add code to link movement x & y

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 
 [RequireComponent(typeof(Rigidbody2D))] //que composant a besoin de rigid body
+[RequireComponent(typeof(Animator))]
 public class PlayerController : MonoBehaviour
 {
     [SerializeField, Range(1f, 20f)]
@@ -10,12 +11,14 @@ public class PlayerController : MonoBehaviour
     
     private Rigidbody2D _rb;
     private Vector2 _movement;
+    private Animator _anim;
     
     private void Awake()
     {
-        Debug.Log("Awake");
+        // Debug.Log("Awake");
         _rb = GetComponent<Rigidbody2D>();
-        Debug.Log(_rb);
+        // Debug.Log(_rb);
+        _anim = GetComponent<Animator>();
     }
 
     private void OnMove(InputValue value)
@@ -26,7 +29,11 @@ public class PlayerController : MonoBehaviour
     private void FixedUpdate()
     {
         _rb.linearVelocity = _movement * speed;
+        
+        _anim.SetFloat("Horizontal", _movement.x);
+        _anim.SetFloat("Vertical", _movement.y);
     }
+    
     /*
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
